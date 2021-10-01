d23aecc7cf768eeca66805363ee8ab13b3847b1d
Tessa Power <tessa@tessapower.co>
2021-10-01 22:19:31 +1300

    Draw bounding box on all buttons

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -21,7 +21,9 @@ void process_image() {
 
     // check if buttons are broken
 
-    // draw red/green box around buttons
+    for (auto const& b : buttons) {
+        draw_bounding_box(b, kColorGreen);
+    }
 }
 
 std::vector<Bounds> find_buttons() {
@@ -68,4 +70,22 @@ void discover_bounds(Coord const& coord, Bounds& discovered) {
 
 pixel_class* get_pixel(Coord const& c) {
     return &picture[c.y][c.x];
+}
+
+void draw_bounding_box(Bounds const& bounds, Color const& color) {
+    // Top and bottom
+    for (int x = bounds.min_x; x < bounds.max_x; ++x) {
+        pixel_class *top = get_pixel(Coord{x, bounds.min_y});
+        pixel_class *bottom = get_pixel(Coord{x, bounds.max_y});
+        top->loaddata(color.R, color.G, color.B);
+        bottom->loaddata(color.R, color.G, color.B);
+    }
+
+    // Left and right
+    for (int y = bounds.min_y; y < bounds.max_y; ++y) {
+        pixel_class *left = get_pixel(Coord{bounds.min_x, y});
+        pixel_class *right = get_pixel(Coord{bounds.max_x, y});
+        left->loaddata(color.R, color.G, color.B);
+        right->loaddata(color.R, color.G, color.B);
+    }
 }
