35c7967d4de3a5abe654a1cd6602332886b24818
Tessa Power <tessa@tessapower.co>
2021-10-01 21:02:08 +1300

    Define a Bounds class to make working with bounding boxes easier

---
+++ include/Bounds.h
@@ -0,0 +1,23 @@
+#ifndef BOUNDS_H
+#define BOUNDS_H
+
+#include "Coord.h"
+
+class Bounds {
+public:
+    Bounds();
+    Bounds(Coord coord);
+    int min_x, max_x, min_y, max_y;
+    inline const int width() const { return max_x - min_x; }
+    inline const int height() const { return max_y - min_y; }
+    void expand_to_include(Coord const& c);
+};
+
+//class Circle {
+//public:
+//    Coord origin;
+//    int radius;
+//    std::vector<Coord> points_on_circumference() const;
+//};
+
+#endif //BOUNDS_H
---
+++ src/Bounds.cpp
@@ -0,0 +1,14 @@
+#include "Bounds.h"
+
+Bounds::Bounds()
+    : min_x{0}, max_x{0}, min_y{0}, max_y{0} {}
+
+Bounds::Bounds(Coord coord)
+    : min_x{coord.x}, max_x{coord.x}, min_y{coord.y}, max_y{coord.y} {}
+
+void Bounds::expand_to_include(Coord const& c) {
+    min_x = std::min(min_x, c.x);
+    max_x = std::max(max_x, c.x);
+    min_y = std::min(min_y, c.y);
+    max_y = std::max(max_y, c.y);
+}
