f52109ad0c5728a8b8aa6fddd40f7002adf1ec1a
Tessa Power <tessa@tessapower.co>
2021-10-03 14:30:16 +1300

    Add temp filename for testing

--- src/io.cpp
+++ src/io.cpp
@@ -8,7 +8,8 @@ void loadButtons() {
     int x, y, R, G, B;
     std::fstream infile;
     std::string infilename, line;
-    infilename = "../tests/Buttons3.ppm";
+    // TODO: Update the filepath to original
+    infilename = "../tests/Buttons.ppm";
     infile.open(infilename.c_str(), std::fstream::in);
     if (infile.is_open() == false) {
         std::cout << "ERROR: not able to open " << infilename << std::endl;
